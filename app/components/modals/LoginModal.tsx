fb896aa1cb46093a857bd8c6a795423386546261
essfadi <o.essfadi@devaxy.com>
2023-08-24 21:25:07 +0100

    :sparkles: login basic functionality

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,20 +1,25 @@
 "use client";
 
-import axios from "axios";
-import { AiFillGithub } from "react-icons/ai";
-import { FcGoogle } from "react-icons/fc";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { toast } from "react-hot-toast";
+import { AiFillGithub } from "react-icons/ai";
+import { FcGoogle } from "react-icons/fc";
 
+import { signIn } from 'next-auth/react';
+
+import useLoginModal from "@/app/hooks/useLoginModal";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
-import Modal from "./Modal";
+import { useRouter } from "next/navigation";
+import Button from "../Button";
 import Heading from "../Heading";
 import Input from "../Inputs/Input";
-import Button from "../Button";
+import Modal from "./Modal";
 
-const RegisterModal = () => {
+const LoginModal = () => {
+  const loginModal = useLoginModal();
   const registerModal = useRegisterModal();
+  const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
   const {
     register,
@@ -22,7 +27,6 @@ const RegisterModal = () => {
     formState: { errors },
   } = useForm<FieldValues>({
     defaultValues: {
-      name: "",
       email: "",
       pasword: "",
     },
@@ -31,30 +35,29 @@ const RegisterModal = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
 
-    axios
-      .post("/api/register", data)
-      .then(() => {
-        registerModal.onClose();
-      })
-      .catch((err) => {
-        toast.error("something Went Wrong");
-      })
-      .finally(() => {
+    signIn('credentials', {
+      ...data,
+      redirect: false
+    })
+      .then((callback) => {
         setIsLoading(false);
-      });
+
+        if (callback?.ok) {
+          toast.success('Logged in!');
+          router.refresh();
+          loginModal.onClose()
+        }
+
+        if (callback?.error) {
+          toast.error(callback.error);
+        }
+
+      })
   };
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Heading title="Welcome to AirBnb" subtitle="Create an account!" />
-      <Input
-        id="name"
-        label="Name"
-        register={register}
-        disabled={isLoading}
-        errors={errors}
-        required
-      />
+      <Heading title="Welcome Back" subtitle="Login to your account!" />
       <Input
         id="email"
         label="Email"
@@ -66,8 +69,8 @@ const RegisterModal = () => {
       />
       <Input
         id="password"
-        label="Password"
         type="password"
+        label="Password"
         register={register}
         disabled={isLoading}
         errors={errors}
@@ -93,8 +96,8 @@ const RegisterModal = () => {
       />
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="justify-center flex flex-row items-center gap-2">
-          <div>Already have an account?</div>
-          <div onClick={registerModal.onClose} className="text-neutral-800 cursor-pointer hover:underline">Log in</div>
+          <div>Don't have an account?</div>
+          <div onClick={loginModal.onClose} className="text-neutral-800 cursor-pointer hover:underline">Register</div>
         </div>
       </div>
     </div>
@@ -103,10 +106,10 @@ const RegisterModal = () => {
   return (
     <Modal
       disabled={isLoading}
-      isOpen={registerModal.isOpen}
-      title="Register"
+      isOpen={loginModal.isOpen}
+      title="Login"
       actionLabel="Continue"
-      onClose={registerModal.onClose}
+      onClose={loginModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
       footer={footerContent}
@@ -114,4 +117,4 @@ const RegisterModal = () => {
   );
 };
 
-export default RegisterModal;
+export default LoginModal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import { Nunito } from 'next/font/google'
 
 import ClientOnly from './components/ClientOnly'
+import LoginModal from './components/modals/LoginModal'
 import RegisterModal from './components/modals/RegisterModal'
 import Navbar from './components/navbar/Navbar'
 import './globals.css'
 import ToasterProvider from './providers/ToasterProvider'
+import getCurrentUser from './actions/getCurrentUser'
 
 export const metadata = {
   title: 'Airbnb',
@@ -15,18 +17,20 @@ const font = Nunito({
   subsets: ['latin'],
 })
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         {children}
       </body>
