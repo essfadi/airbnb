a96229dd8b1fdd869a9d5c6b272e4b72a0062d52
essfadi <o.essfadi@devaxy.com>
2023-08-28 18:59:44 +0100

    :sparkles: favoriting functionality added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
+import getListings from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+
+import type { Listing } from "@prisma/client";
+import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
 
-export default function Home() {
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+  
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    )
+  }
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+         {listings.map((listing: Listing) => {
+          return (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          )
+         })}
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
