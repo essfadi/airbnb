860779f8c52e048612a562645cf20505cf624b08
essfadi <o.essfadi@devaxy.com>
2023-08-31 15:32:51 +0100

    :bug: static rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,54 @@
-import getListings, { IListingsParams } from "./actions/getListings";
-import ClientOnly from "./components/ClientOnly";
-import Container from "./components/Container";
-import EmptyState from "./components/EmptyState";
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
+import EmptyState from "@/app/components/EmptyState";
 
-import getCurrentUser from "./actions/getCurrentUser";
-import ListingCard from "./components/listings/ListingCard";
-import { SafeListing } from "./types";
+import getListings, { 
+  IListingsParams
+} from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import ClientOnly from "./components/ClientOnly";
 
 interface HomeProps {
   searchParams: IListingsParams
-}
+};
+
+export const dynamic = 'force-static';
 
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
-  
+
   if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
       </ClientOnly>
-    )
+    );
   }
 
   return (
     <ClientOnly>
       <Container>
-        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-         {listings.map((listing: SafeListing) => {
-          return (
+        <div 
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
             <ListingCard
               currentUser={currentUser}
               key={listing.id}
               data={listing}
             />
-          )
-         })}
+          ))}
         </div>
       </Container>
     </ClientOnly>
